cc033a8f1778a464e88daba8a99b5f7238023c91
syugar <redsarabled@gmail.com>
2020-10-16 15:43:33 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,15 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      #もしもwday_numが7以上であれば、7を引く
+      if @todays_date.wday + x < 7   
+        wx = @todays_date.wday + x
+      else # +xをする
+        wx = @todays_date.wday + x - 7
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wx]}
       @week_days.push(days)
     end
   end
